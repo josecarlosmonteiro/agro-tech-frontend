8c588624901ea33e33bf30e7d585747b1a89ace1
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2026-04-12 17:42:56 -0300

    feat: implementar rota de alertas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Dashboard } from './pages/dashboard/dashboard';
 import { NotFounded } from './pages/not-founded/not-founded';
 import { Areas } from './pages/areas/areas';
 import { Sensors } from './pages/sensors/sensors';
+import { Alerts } from './pages/alerts/alerts';
 
 export const routes: Routes = [
   { path: 'dashboard', component: Dashboard },
   { path: 'areas', component: Areas },
   { path: 'sensores', component: Sensors },
+  { path: 'alertas', component: Alerts },
   { path: '**', component: NotFounded },
 ];
