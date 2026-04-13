d044f9501ec1761a1649a2a8f678b9d0993db577
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2026-04-13 13:15:05 -0300

    feat: implementar ajuste de retorno de http.

--- src/app/services/sensors/sensor-service.ts
+++ src/app/services/sensors/sensor-service.ts
@@ -4,6 +4,7 @@ import { Observable, tap } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { listUtil } from '../../utils/lists';
 import { baseUrl } from '../../constants/url';
+import { HttpResponseModel } from '../../models/http/http-response.model';
 
 @Injectable({
   providedIn: 'root',
@@ -15,20 +16,20 @@ export class SensorService {
   #sensorsSignal = signal<SensorModel[]>([]);
   public sensors = this.#sensorsSignal.asReadonly();
 
-  findAll(): Observable<SensorModel[]> {
+  findAll(): Observable<HttpResponseModel<SensorModel[]>> {
     return this.http
-      .get<SensorModel[]>(this.url)
-      .pipe(tap(result => this.#sensorsSignal.set(result)));
+      .get<HttpResponseModel<SensorModel[]>>(this.url)
+      .pipe(tap(result => this.#sensorsSignal.set(result.content)));
   }
 
   onCreateSensor(newSensor: SensorModel) {
     this.#sensorsSignal.update(previous => listUtil.add(previous, newSensor));
   }
 
-  create(newSensor: Partial<SensorModel>): Observable<SensorModel> {
-    return this.http.post<SensorModel>(this.url, newSensor).pipe(
+  create(newSensor: Partial<SensorModel>): Observable<HttpResponseModel<SensorModel>> {
+    return this.http.post<HttpResponseModel<SensorModel>>(this.url, newSensor).pipe(
       tap(result => {
-        this.onCreateSensor(result);
+        this.onCreateSensor(result.content);
       }),
     );
   }
@@ -37,10 +38,10 @@ export class SensorService {
     this.#sensorsSignal.update(previous => listUtil.update(previous, newSensor, 'id'));
   }
 
-  update(newData: Partial<SensorModel>): Observable<SensorModel> {
-    return this.http.patch<SensorModel>(`${this.url}/${newData.id}`, newData).pipe(
+  update(newData: Partial<SensorModel>): Observable<HttpResponseModel<SensorModel>> {
+    return this.http.patch<HttpResponseModel<SensorModel>>(`${this.url}/${newData.id}`, newData).pipe(
       tap(result => {
-        this.onUpdateSensor(result);
+        this.onUpdateSensor(result.content);
       }),
     );
   }
